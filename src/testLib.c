56bbbda4e031154d221185fe676656131bca6589
WookiesRpeople2 <keenantobiansky@gmail.com>
2023-12-15 02:02:34 +0100

    updated testLib

--- src/testLib.c
+++ src/testLib.c
@@ -3,9 +3,15 @@
 
 int main(int argc, char **argv)
 {
-  if (argc > 0)
+  if (argc > 1)
   {
     printf("%s\n", argv[1]);
     printf("%s\n", reverse(argv[1]));
   }
+  else
+  {
+    printf("No command-line arguments provided.\n");
+  }
+
+  return 0;
 }
